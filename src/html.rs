5775f268529dde3b67bc65525ee25b704652ea80
Shannon Rothe <shannon.michael.rothe@gmail.com>
2022-01-05 22:22:01 +1000

    changes around feedback

--- src/html.rs
+++ src/html.rs
@@ -1,34 +1,114 @@
+use std::collections::HashMap;
 use std::fmt::Display;
 
-trait HtmlTag {
-    fn opening_tag(&self) -> String;
-    fn closing_tag(&self) -> String;
+#[derive(Clone, Debug, PartialEq)]
+pub enum Node {
+    Heading(u8),
+    Paragraph,
+    Text(String),
+    Strong(String),
+    Em(String),
+
+    Code(String),
+
+    OrderedList,
+    UnorderedList,
+    ListItem(Vec<Node>),
 }
 
-#[derive(Debug, PartialEq)]
-pub struct Tag {
-    pub name: String,
-    pub text: String,
-}
+impl Node {
+    pub fn render(&self, classes: &HashMap<String, String>) -> String {
+        let root_classes = &classes.get(&self.to_string());
 
-impl HtmlTag for Tag {
-    fn opening_tag(&self) -> String {
-        format!("<{}>", self.name)
+        match self {
+            Node::Text(text) => text.to_string(),
+            Node::Code(code) => self.tag(&classes.get(&self.to_string()), code.to_string()),
+            Node::ListItem(nodes) => {
+                let children: Vec<String> = nodes.iter().map(|n| n.render(classes)).collect();
+                self.tag(root_classes, children.join(""))
+            }
+            Node::Em(text) => self.tag(root_classes, text.to_string()),
+            Node::Strong(text) => self.tag(root_classes, text.to_string()),
+            _ => "".to_string(),
+        }
+    }
+
+    fn tag(&self, classes: &Option<&String>, text: String) -> String {
+        format!(
+            "{}{}{}",
+            self.opening_tag(classes),
+            text,
+            self.closing_tag()
+        )
+    }
+
+    fn opening_tag(&self, classes: &Option<&String>) -> String {
+        let mut tag = format!("<{}", self.to_string());
+
+        if let Some(classes) = classes {
+            if !classes.is_empty() {
+                tag.push_str(format!(" class=\"{}\"", classes).as_str());
+            }
+        }
+
+        tag.push('>');
+        tag
     }
 
     fn closing_tag(&self) -> String {
-        format!("</{}>", self.name)
+        format!("</{}>", self.to_string())
     }
+}
+
+impl Display for Node {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let str = match self {
+            Node::Text(text) => text.to_string(),
+            Node::Paragraph => "p".to_string(),
+            Node::Code(_) => "code".to_string(),
+            Node::Heading(level) => format!("h{}", level),
+            Node::Strong(_) => "strong".to_string(),
+            Node::Em(_) => "em".to_string(),
+            Node::OrderedList => "ol".to_string(),
+            Node::UnorderedList => "ul".to_string(),
+            Node::ListItem(_) => "li".to_string(),
+        };
+
+        write!(f, "{}", str)
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Tag {
+    pub node: Node,
+    pub children: Vec<Node>,
 }
 
 impl Tag {
-    pub fn as_html(&self) -> String {
-        vec![self.opening_tag(), self.text.clone(), self.closing_tag()].join("")
+    pub fn as_html(&self, classes: &HashMap<String, String>) -> String {
+        let children: Vec<String> = self.children.iter().map(|n| n.render(classes)).collect();
+        vec![
+            self.opening_tag(&classes.get(&self.node.to_string())),
+            children.join(""),
+            self.closing_tag(),
+        ]
+        .join("")
     }
-}
 
-impl Display for Tag {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.as_html())
+    pub fn opening_tag(&self, classes: &Option<&String>) -> String {
+        let mut tag = format!("<{}", self.node.to_string());
+
+        if let Some(classes) = &classes {
+            if !classes.is_empty() {
+                tag.push_str(format!(" class=\"{}\"", classes).as_str());
+            }
+        }
+
+        tag.push('>');
+        tag
+    }
+
+    pub fn closing_tag(&self) -> String {
+        format!("</{}>\n", self.node.to_string())
     }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,47 +1,158 @@
-use crate::html::Tag;
-use crate::token::Token;
+use crate::html::{Node, Tag};
+use crate::lexer::Lexer;
+use crate::token::{ListType, Token};
 
 pub type Program = Vec<Tag>;
 
-fn heading_pattern_to_name(pattern: &str) -> String {
-    format!("h{}", pattern.chars().count())
-}
-
 #[derive(Debug)]
 pub struct Parser {
-    tokens: Vec<Token>,
+    lexer: Lexer,
+    ast: Program,
 }
 
 impl Parser {
-    pub fn new(tokens: Vec<Token>) -> Self {
-        Self { tokens }
+    pub fn new(lexer: Lexer) -> Self {
+        Self {
+            lexer,
+            ast: Program::new(),
+        }
     }
 
-    pub fn parse(&mut self) -> Result<Program, String> {
-        let mut program = Vec::new();
-
-        for token in self.tokens.iter() {
+    pub fn parse(&mut self) -> Program {
+        while let Some(token) = self.lexer.next() {
             match token {
-                Token::Heading((pattern, text)) => {
-                    program.push(Tag {
-                        name: heading_pattern_to_name(pattern),
-                        text: String::from(text),
-                    });
-                }
-                Token::Paragraph(text) => {
-                    program.push(Tag {
-                        name: String::from("p"),
-                        text: String::from(text),
-                    });
-                }
-                Token::Code(code) => program.push(Tag {
-                    name: String::from("code"),
-                    text: String::from(code),
-                }),
+                Token::Heading(text) => self.parse_heading(text),
+                Token::Text(text) => self.parse_text(Node::Text(text)),
+                Token::Code(text) => self.parse_code(text),
+                Token::Strong(text) => self.parse_text(Node::Strong(text)),
+                Token::Em(text) => self.parse_text(Node::Em(text)),
+                Token::List(list_type) => self.parse_list(list_type),
+                Token::NewLine => {}
             }
         }
 
-        Ok(program)
+        self.ast.to_vec()
+    }
+
+    fn parse_heading(&mut self, text: String) {
+        let level = text.chars().count() as u8;
+
+        match self.lexer.next() {
+            Some(Token::Text(text)) => self.ast.push(Tag {
+                node: Node::Heading(level),
+                children: vec![Node::Text(text)],
+            }),
+            Some(_) => panic!(""),
+            None => panic!(""),
+        }
+    }
+
+    fn parse_code(&mut self, text: String) {
+        let t = text.clone();
+        self.ast.push(Tag {
+            node: Node::Code(text),
+            children: vec![Node::Text(t)],
+        });
+    }
+
+    fn parse_text(&mut self, initial: Node) {
+        let mut parent = Tag {
+            node: Node::Paragraph,
+            children: vec![initial],
+        };
+
+        while let Some(token) = self.lexer.next() {
+            match token {
+                Token::NewLine => break,
+                initial => {
+                    let mut nodes = self.parse_inline_nodes(&initial);
+                    parent.children.append(&mut nodes);
+                }
+            }
+        }
+
+        self.ast.push(parent)
+    }
+
+    fn parse_list(&mut self, list_type: ListType) {
+        let tag = match list_type {
+            ListType::Ordered => Tag {
+                node: Node::OrderedList,
+                children: self.parse_list_items(),
+            },
+            ListType::Unordered => Tag {
+                node: Node::UnorderedList,
+                children: self.parse_list_items(),
+            },
+        };
+
+        self.ast.push(tag);
+    }
+
+    fn parse_list_items(&mut self) -> Vec<Node> {
+        let mut list_items = Vec::new();
+        let mut nodes = Vec::new();
+
+        while let Some(token) = self.lexer.next() {
+            match token {
+                Token::NewLine => {
+                    list_items.push(Node::ListItem(nodes.clone()));
+                    nodes.clear();
+
+                    if let Some(token) = self.lexer.next() {
+                        if token == Token::NewLine {
+                            break;
+                        }
+                    }
+                }
+                Token::List(_) => {
+                    list_items.append(&mut self.parse_list_items());
+                }
+                _ => {
+                    if let Some(inline_node) = self.parse_inline_node(&token) {
+                        nodes.push(inline_node);
+                    }
+                }
+            }
+        }
+
+        if !nodes.is_empty() {
+            list_items.push(Node::ListItem(nodes.clone()));
+            nodes.clear();
+        }
+
+        list_items
+    }
+
+    /// Parse all inline tokens into nodes until a new line is hit.
+    fn parse_inline_nodes(&mut self, initial: &Token) -> Vec<Node> {
+        let mut nodes = Vec::new();
+
+        if let Some(node) = self.parse_inline_node(initial) {
+            nodes.push(node);
+        }
+
+        while let Some(token) = self.lexer.next() {
+            if let Some(node) = self.parse_inline_node(&token) {
+                nodes.push(node);
+            }
+
+            if token == Token::NewLine {
+                break;
+            }
+        }
+
+        nodes
+    }
+
+    fn parse_inline_node(&mut self, token: &Token) -> Option<Node> {
+        match token {
+            Token::Text(text) => Some(Node::Text(text.to_string())),
+            Token::Code(text) => Some(Node::Code(text.to_string())),
+            Token::Strong(text) => Some(Node::Strong(text.to_string())),
+            Token::Em(text) => Some(Node::Em(text.to_string())),
+            _ => None,
+        }
     }
 }
 
@@ -50,23 +161,23 @@ mod tests {
     use super::*;
 
     #[test]
-    fn parses_tags() {
+    fn parses_list() {
         assert_eq!(
-            Parser::new(vec![
-                Token::Heading(("#".to_string(), "Hello World".to_string())),
-                Token::Paragraph("This is the body".to_string())
-            ])
-            .parse(),
-            Ok(vec![
+            Parser::new(Lexer::new("some text followed by\n* item 1\n* item 2").into_iter())
+                .parse(),
+            vec![
                 Tag {
-                    name: "h1".to_string(),
-                    text: "Hello World".to_string()
+                    node: Node::Paragraph,
+                    children: vec![Node::Text("some text followed by".to_string())]
                 },
                 Tag {
-                    name: "p".to_string(),
-                    text: "This is the body".to_string()
+                    node: Node::UnorderedList,
+                    children: vec![
+                        Node::ListItem(vec![Node::Text("item 1".to_string())]),
+                        Node::ListItem(vec![Node::Text("item 2".to_string())])
+                    ]
                 }
-            ]),
-        )
+            ]
+        );
     }
 }
